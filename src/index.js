2a5012fb6b1ab704fa878251da098d4b9828a8c1
jessicaklinc <jessicaklinc@hotmail.com>
2019-02-19 06:35:50 +0100

    first update

--- src/index.js
+++ src/index.js
@@ -2,20 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'X',
+    };
+  }
   render() {
     return (
-      <button className="square">
-        {/* TODO 1*/}
+      <button 
+      className="square" 
+      onClick={()=> {
+        this.props.onClick();
+      }}>
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
+  constructor(props) {
+    super(props);
+    this.state ={
+      squares: Array(9).fill(null),
+    };
   }
-
+  renderSquare(i) {
+    return <Square  
+    value={this.state.squares[i]}
+    onClick={()=> this.handleClick(i)}/>;
+  }
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
   render() {
     const status = 'Next player: X';
 
